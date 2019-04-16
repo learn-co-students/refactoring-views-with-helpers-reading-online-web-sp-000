65135d8fc9df22fd62f44a62b4458182b0f24313
Rafael Batista <rafaelb24@yahoo.com>
2019-04-16 17:03:00 +0000

    Done.

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -2,4 +2,19 @@ class AuthorsController < ApplicationController
   def show
     @author = Author.find(params[:id])
   end
+  
+  if RUBY_VERSION>='2.6.0'
+  if Rails.version < '5'
+    class ActionController::TestResponse < ActionDispatch::TestResponse
+      def recycle!
+        # hack to avoid MonitorMixin double-initialize error:
+        @mon_mutex_owner_object_id = nil
+        @mon_mutex = nil
+        initialize
+      end
+    end
+  else
+    puts "Monkeypatch for ActionController::TestResponse no longer needed"
+  end
+end
 end
