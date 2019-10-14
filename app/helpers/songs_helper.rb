50cb8a02555c8f653bd5c40bca32acb8dff76682
Brandon Teich <brandonteich123@gmail.com>
2019-10-14 23:13:30 +0000

    Automatically backed up by Learn

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,4 +1,5 @@
 module SongsHelper
+<<<<<<< HEAD
   def display_artist(song)
     if song.artist
       link_to song.artist.name, artist_path(song.artist)
@@ -6,4 +7,7 @@ module SongsHelper
       link_to "Add Artist", edit_song_path(song)
     end
   end
+=======
+
+>>>>>>> 8a0dd86299237553a353123ed935143f23529199
 end 
