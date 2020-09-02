f27fd66ee2b874aea548814dc7849cbee416fca5
Juan Carlo Banayo <johncban@gmail.com>
2020-09-02 01:11:32 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,4 @@
 class Post < ActiveRecord::Base
-
   validate :is_title_case
   before_validation :make_title_case
   belongs_to :author
@@ -15,4 +14,5 @@ class Post < ActiveRecord::Base
   def make_title_case
     self.title = self.title.titlecase
   end
+
 end
